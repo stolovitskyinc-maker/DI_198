782cd12b8c3535c2eb506f5d31a36f4afc4a6e1a
Yaakov Stolovitsky <stolovitskyinc@gmail.com>
2026-05-17 16:52:05 +0300

    ADD basic

---
+++ Week-1/Day-1/ExerciseXP/Exercise4.py
@@ -0,0 +1,3 @@
+computer_brand = "mac"
+
+print(f"I have a {computer_brand} computer.")
---
+++ Week-1/Day-1/ExerciseXP/exercise5.py
@@ -0,0 +1,13 @@
+# Create a variable called name, and set it’s value to your name.
+# Create a variable called age, and set it’s value to your age.
+# Create a variable called shoe_size, and set it’s value to your shoe size.
+# Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2, and 3.
+# Have your code print the info message.
+# Run your code.
+
+name = "Yaakov"
+age = 32
+shoe_size = 42
+info = f"Hi my name is {name} i am {age} years old and i my shoes is size {shoe_size}."
+
+print(info)
