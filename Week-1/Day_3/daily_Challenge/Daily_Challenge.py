6649a4c9f2f1847aab19af92bac34a0d495daf53
Yaakov Stolovitsky <stolovitskyinc@gmail.com>
2026-05-19 14:50:07 +0300

    ADD DI_198

---
+++ Week-1/Day_3/daily_Challenge/Daily_Challenge.py
@@ -0,0 +1,88 @@
+# (1.) Create a dictionary that stores the indices (number of the position) of each letter in a word provided by the user(input()).
+
+# Key Python Topics:
+
+# User input (input())
+# Dictionaries
+# Loops (for loop)
+# Conditional statements (if, else)
+# String manipulation
+# Lists
+
+# Instructions:
+# 1. User Input:
+
+# Ask the user to enter a word.
+# Store the input word in a variable.
+# 2. Creating the Dictionary:
+
+# Iterate through each character of the input word using a loop.
+# And check if the character is already a key in the dictionary.
+
+#     * If it is, append the current index to the list associated with that key.
+#     * If it is not, create a new key-value pair in the dictionary.
+# Ensure that the characters (keys) are strings.
+# Ensure that the indices (values) are stored in lists.
+# 3. Expected Output:
+
+# For the input “dodo”, the output should be: {"d": [0, 2], "o": [1, 3]}.
+# For the input “froggy”, the output should be: {"f": [0], "r": [1], "o": [2], "g": [3, 4], "y": [5]}.
+# For the input “grapes”, the output should be: {"g": [0], "r": [1], "a": [2], "p": [3], "e": [4], "s": [5]}.
+user_word = input("Please enter a word: ")
+
+char_indices = {}
+
+for index, char in enumerate(user_word):
+    char_str = str(char)
+
+    if char_str in char_indices:
+        char_indices[char_str].append(index)
+
+    else:
+        char_indices[char_str] = [index]
+
+print(char_indices)
+
+# (2.) 1. Store Data:
+
+# You will be provided with a dictionary (items_purchase) where the keys are the item names and the values are their prices (as strings with a dollar sign). The priority is defined by the position of the iten on the dictionary: from the most important to the less important.
+# You will also be given a string (wallet) representing the amount of money you have.
+# 2. Data Cleaning:
+
+# You need to clean the dollar sign and the commas using python. Don’t hard code it.
+# 3. Determining Affordable Items:
+
+# create a list called basket and add there the items that you can buy with the money you have on the wallet
+# Don’t forget to update the wallet after buying an item.
+# If the basket is empty (no items can be afforded), return the string “Nothing”.
+# Otherwise, print the basket list in alphabetical order.
+# 4. Examples:
+
+# Given:
+# items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
+# wallet = "$300"
+
+items_purchase = {
+    "Water": "$1",
+    "Bread": "$3",
+    "TV": "$1,000",
+    "Fertilizer": "$20"
+    }
+wallet_str = "$300"
+
+wallet = float(wallet_str.replace("$", "").replace(",", ""))
+
+basket = []
+
+for item, price_str in items_purchase.items():
+    price = float(price_str.replace("$", "").replace(",", ""))
+
+    if wallet >= price:
+        basket.append(item)
+        wallet -= price
+
+if not basket:
+    print("Nothing")
+else:
+    basket.sort()
+    print(basket)
