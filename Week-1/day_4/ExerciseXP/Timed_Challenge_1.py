5d2fb95aab5c9d2e652294ea4e541aaffe16ead4
Yaakov Stolovitsky <stolovitskyinc@gmail.com>
2026-05-21 11:57:29 +0300

    ADD DI_198

---
+++ Week-1/day_4/ExerciseXP/Timed_Challenge_1.py
@@ -0,0 +1,9 @@
+# 1. Take the string and character as input from the user
+user_string = input("Enter the string: ")
+target_char = input("Enter the character to count: ")
+
+# 2. Use the built-in count function to find the occurrences
+occurrence_count = user_string.count(target_char)
+
+# 3. Print out the final number result
+print(occurrence_count)
