c3aa07c3d83e85928e9b7f3cb9666e4f24e48e7f
Yaakov Stolovitsky <stolovitskyinc@gmail.com>
2026-06-01 11:02:35 +0300

    ADD DI_198

---
+++ week_3/Day_2/Functions/script.js
@@ -0,0 +1,47 @@
+// // // simple function
+// function helloWorld (args){
+//     console.log("helloWorld");
+//     return 1;
+// }
+// // //function call
+// helloWorld();
+
+// //params
+// function sayHiToUser(userame) {
+//     console.log('Hello, ${username}!')
+// }
+
+// sayHiToUser('Yaakov')
+
+// // return
+// function factorialize(num){
+//     newNumber = 1
+//     for (let i=1; i <= num; i++) {
+//         newNumber *= i
+//     }
+
+//     return newNumber
+// }
+
+// plusOne = factorialize(5) +1;
+// console.log(plusOne);
+
+
+// // Different Types of functions
+// //function expression
+// const myFunc = function (a, b) {
+//     return a**b;
+
+// };
+
+// console.log(myFunc(9, 3));
+
+// //1 linner arrow function
+// const yourFunc2 = (x, y) => x**y;
+
+// console.log(yourFunc2(12, 8));
+
+// // IIFE - Immediately Invoked Function Expression
+// (function addStrings(a, b) {
+//     console.log(a + b);
+// })('chicken', 'nuggets');
