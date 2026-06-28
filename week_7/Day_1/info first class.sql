ab7f3990ce78fe89cb309472f74de1aa23b72ff2
Yaakov Stolovitsky <stolovitskyinc@gmail.com>
2026-06-28 15:10:53 +0300

    ADD DI_198

---
+++ week_7/Day_1/info first class.sql
@@ -0,0 +1,38 @@
+insert into dummy_table values 
+	('DEF', 'location-D', 40), 
+	('PQR', 'location-G', 35),
+	('XYZ', 'location-H', 30)
+	
+
+
+--select
+SELECT * FROM dummy_table
+
+-- DDL - Data Definition - Create, Alter, Drop
+-- DML - Data Manipulation - Insert, Selecr, Update, Delete
+-- DCL - Data COntrol, permision - Grant, Revoke
+
+--Filter
+--where - select, update, delete
+
+--BETWEEN and IN
+--LIKE
+--LIMIT
+--ORDER BY
+
+--Aggregate Function
+--count
+--AVG
+--MIN
+--MAX
+--SUM
+
+--REMOVE DUPLICATE
+--DISTINCT
+
+--IS NULL
+
+select count(*) from student
+
+--ALTER
+
